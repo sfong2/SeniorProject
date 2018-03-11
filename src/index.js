7abe6834961449b5e90c6f203f3ea7507a97037e
sfong2 <sfong2@uci.edu>
2018-03-11 13:32:49 -0700

    "Alternate Login"

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ import App from './App';
 //ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
 
-import {render} from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {render} from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 render((
+    <Provider store={store}>
     <BrowserRouter>
         <App />
     </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
