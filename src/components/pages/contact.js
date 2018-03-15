b15ed6a9261f3b7e6bfdcf2b65929e1d36db213d
sfong2 <sfong2@uci.edu>
2018-03-15 14:14:19 -0700

    some minor changes

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,22 +1,10 @@
 import React from 'react';
 
-/*
-class Contact extends Component {
-  render() {
-    return (
-        <div className = "contact">
-            Welcome to CJS Connection's Contact Page!
-        </div>
-    );
-  }
-}
-*/
-
 const Contact = () => (
   <div className = "container-fluid">
     <h1> Contact Us</h1>
       <h4>Please contact the right department for you.</h4>
-      <h3> Login</h3>
+      <h3> Website</h3>
        <h4><b>Email:</b><a hef= "https://www.google.com/gmail/" target = "_blank"> sfong2@uci.edu</a></h4>
       <h3> Data Management</h3>
         <h4><b>Email:</b><a hef= "https://www.google.com/gmail/" target = "_blank"> junl14@uci.edu</a></h4>
