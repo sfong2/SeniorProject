48235dc1035434c9225bec8a2b5d44b46408087e
sfong2 <sfong2@uci.edu>
2018-03-15 19:36:08 -0700

    fixed contacts a bit

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -5,11 +5,11 @@ const Contact = () => (
     <h1> Contact Us</h1>
       <h4>Please contact the right department for you.</h4>
       <h3> Website</h3>
-       <h4><b>Email:</b><a hef= "https://www.google.com/gmail/" target = "_blank"> sfong2@uci.edu</a></h4>
+       <h4><b>Email: </b><a href="https://www.google.com/gmail/" class="text-primary">sfong2@uci.edu</a></h4>
       <h3> Data Management</h3>
-        <h4><b>Email:</b><a hef= "https://www.google.com/gmail/" target = "_blank"> junl14@uci.edu</a></h4>
+        <h4><b>Email: </b><a href="https://www.google.com/gmail/" class="text-primary">junl14@uci.edu</a></h4>
       <h3> Information Display or Other Questions </h3>
-        <h4><b>Email:</b><a hef= "https://www.google.com/gmail/" target = "_blank"> cgorgoni@uci.edu</a></h4>
+        <h4><b>Email: </b><a href="https://www.google.com/gmail/" class="text-primary">cgorgoni@uci.edu</a></h4>
   </div>
 )
 
