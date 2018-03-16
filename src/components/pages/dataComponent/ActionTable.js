511b69ad626eaf87d74ec902fe3543108fdc7c32
junlico <junl14@uci.edu>
2018-03-16 01:34:58 -0700

    action table remove

--- src/components/pages/dataComponent/ActionTable.js
+++ src/components/pages/dataComponent/ActionTable.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import XLSX from 'xlsx';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 const headers = ["Campaign Name", "Campaign Daily Budget", "Campaign Start Date", "Campaign End Date", "Campaign Targeting Type",	"Ad Group Name", "Max Bid", "SKU", "Keyword", "Match Type", "Campaign Status", "Ad Group Status", "Status", "Bid+"];
@@ -14,6 +15,7 @@ export default class ActionTable extends Component {
     this.renderEditable = this.renderEditable.bind(this);
     this.make_act_col = this.make_act_col.bind(this);
     this.make_cols = this.make_cols.bind(this);
+    this.onClickDownloadFile = this.onClickDownloadFile.bind(this);
   }
 
   componentDidMount = () => {
@@ -27,6 +29,8 @@ export default class ActionTable extends Component {
     }
     if (header === "Keyword") {
       c["accessor"] = "Customer Search Term";
+      c["Cell"] = this.renderEditable;
+    } else if (header === "Match Type") {
       c["Cell"] = this.renderEditable;
     }
     return c;
@@ -57,14 +61,44 @@ export default class ActionTable extends Component {
       />
     );
   }
+
+  onClickDownloadFile = () => {
+    /* convert state to workbook */
+    const ws = XLSX.utils.aoa_to_sheet(this.state.data);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
+    /* generate XLSX file and send to client */
+    XLSX.writeFile(wb, "sheetjs.xlsx")
+  }
+
   render() {
-    console.log(this.props.data)
     return (
       <div>
-        <button className="btn btn-success">Download Operation File</button>
+        <button className="btn btn-success" onClick={this.onClickDownloadFile}>Download Operation File</button>
         <ReactTable
           data={this.state.data}
-          columns={this.state.columns}
+          columns={[{
+            Header: "Action",
+            accessor: "action",
+            Cell: ({value}) => (<button style={{width: "100%", height: "100%"}} onClick={this.getTdProps}>Remove</button>)}
+          ].concat(this.state.columns)}
+          getTdProps={(state, rowInfo, column, instance) => {
+                return {
+                  onClick: (e, handleOriginal) => {
+                    if (rowInfo) {
+                      let id = column.id, row = rowInfo.original;
+                      if (id === "action") {
+                        let data = this.state.data;
+                        data = data.filter(item => item.__rowNum__ !== row.__rowNum__);
+                        this.setState({data: data})
+                      }
+                    }
+                    if (handleOriginal) {
+                      handleOriginal()
+                    }
+                  }
+                }
+              }}
         />
       </div>
     )
